da5b615a6e59bdc681bf31937a475835924acdf2
Jerome Tabiri <jtabiri@gmail.com>
2021-12-27 10:35:38 -0500

    added missing addons to custom_addons

--- pre-prod/output.tf
+++ pre-prod/output.tf
@@ -1,3 +1,8 @@
 output "elb_dns_name" {
   value = aws_lb.odoo.dns_name
 }
+
+output "odoo_url" {
+  description = "Odoo URL"
+  value       = aws_route53_record.ssxodoo.name
+}
--- pre-prod/terraform.tfvars
+++ pre-prod/terraform.tfvars
@@ -1,2 +1,3 @@
 region   = "eu-central-1"
 tag_name = "ssx-preprod"
+key_name = "odoo-key-eu"
