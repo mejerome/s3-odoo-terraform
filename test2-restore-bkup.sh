e6bd0141bfef3a2a372dbcca0613330dc024edc0
Jerome Tabiri <jtabiri@gmail.com>
2022-01-26 18:09:06 -0500

    fixed backup script to s3 bucket

--- test2-restore-bkup.sh
+++ test2-restore-bkup.sh
@@ -4,11 +4,12 @@ BACKUP_DIR=/home/bitnami/odoo_backup
 ODOO_DB=test2-restore
 ADMIN_PASSWORD=bitnami
 
-curl -X POST -F 'master_pwd=${ADMIN_PASSWORD}' \
-  -F 'name=${ODOO_DB}' \
-  -F 'backup_format=zip' \
-  -o ${BACKUP_DIR}/${ODOO_DB}-$(date +%Y.%m.%d-%H.%M).zip \
-  http://localhost:8069/web/database/backup
+curl -X POST \
+    -F "master_pwd=${ADMIN_PASSWORD}" \
+    -F "name=${ODOO_DB}" \
+    -F "backup_format=zip" \
+    -o ${BACKUP_DIR}/${ODOO_DB}.$(date +"%Y-%m-%d-%H-%M-%S").zip \
+    http://localhost:8069/web/database/backup
 
 cd ${BACKUP_DIR}; cp $(ls -t | head -1) restore.zip
 aws s3 sync ${BACKUP_DIR} s3://ssx-postgres-backups/ssx-backups
