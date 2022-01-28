f47935554bf64b800d67edf250b9958cb1321f2b
Jerome Tabiri <jtabiri@gmail.com>
2022-01-28 09:59:49 -0500

    corrected backup scripts

--- test2-restore-bkup.sh
+++ test2-restore-bkup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # variables
-BACKUP_DIR=/home/bitnami/odoo_backup
+BACKUP_DIR=~/odoo_backup
 ODOO_DB=test2-restore
 ADMIN_PASSWORD=bitnami
 
@@ -8,7 +8,7 @@ curl -X POST \
     -F "master_pwd=${ADMIN_PASSWORD}" \
     -F "name=${ODOO_DB}" \
     -F "backup_format=zip" \
-    -o ${BACKUP_DIR}/${ODOO_DB}.$(date +"%Y-%m-%d-%H-%M-%S").zip \
+    -o ${BACKUP_DIR}/${ODOO_DB}.$(date +%F)-$(date +%T).zip \
     http://localhost:8069/web/database/backup
 
 cd ${BACKUP_DIR}; cp $(ls -t | head -1) restore.zip
