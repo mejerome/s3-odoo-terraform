d4010958b97164ed7b7207286ce8a5857d6dfaf5
Jerome Tabiri <jtabiri@gmail.com>
2022-02-06 15:08:23 -0500

    first commit

---
+++ production/acm.tf
@@ -0,0 +1,62 @@
+resource "aws_acm_certificate" "ssx" {
+  domain_name       = aws_route53_record.ssxodoo.name
+  validation_method = "DNS"
+  # subject_alternative_names = ["finance.internal.secondstax.com"]
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_acm_certificate" "ssxfinance" {
+  domain_name       = "finance.internal.secondstax.com"
+  validation_method = "EMAIL"
+}
+
+resource "aws_route53_record" "ssxodoo" {
+  zone_id = var.hosted_zone_id
+  name    = var.hosted_zone_name
+  type    = "CNAME"
+  ttl     = "300"
+
+  records = [
+    aws_lb.odoo-lb.dns_name,
+  ]
+
+  depends_on = [
+    aws_lb.odoo-lb,
+  ]
+}
+
+resource "aws_route53_record" "cert_validation_record" {
+  zone_id = var.hosted_zone_id
+  for_each = {
+    for dvo in aws_acm_certificate.ssx.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+}
+
+resource "aws_acm_certificate_validation" "ssx" {
+  certificate_arn         = aws_acm_certificate.ssx.arn
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation_record : record.fqdn]
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_acm_certificate_validation" "ssxfinance" {
+  certificate_arn         = aws_acm_certificate.ssxfinance.arn
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
---
+++ production/variables.tf
@@ -0,0 +1,43 @@
+variable "tag_name" {
+  description = "Deployment tag name"
+  type        = string
+}
+
+variable "odoo_ami" {
+  description = "AMI ID"
+  type        = string
+}
+variable "db_username" {
+  type        = string
+  description = "odoo database username"
+}
+
+variable "db_password" {
+  type        = string
+  description = "odoo database password"
+}
+
+variable "region" {
+  type        = string
+  description = "AWS region"
+}
+
+variable "key_name" {
+  type        = string
+  description = "AWS key name"
+  default     = "odoo-key"
+}
+
+variable "key_file" {
+  default = "../../odoo-key.pem"
+}
+
+variable "hosted_zone_id" {
+  type        = string
+  description = "AWS DNS zone name"
+}
+
+variable "hosted_zone_name" {
+  type        = string
+  description = "AWS DNS zone name"
+}
