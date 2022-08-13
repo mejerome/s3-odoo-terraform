856198dcae718f60abfdb8ab605fb63f0bf1c580
Jerome Tabiri <jtabiri@gmail.com>
2022-08-13 16:08:57 -0400

    update security group for testing 8068

--- production/securitygroups.tf
+++ production/securitygroups.tf
@@ -80,3 +80,29 @@ resource "aws_security_group" "ssh" {
     ipv6_cidr_blocks = ["::/0"]
   }
 }
+
+resource "aws_security_group" "allow_syslog" {
+  name        = "allow_syslog"
+  description = "Allow Syslog inbound traffic"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    description = "ssh from VPC"
+    from_port   = 8068
+    to_port     = 8068
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = var.tag_name
+  }
+}
